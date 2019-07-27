69b8e46d5028e2a735896324c9bced636992055c
git4robot <49167781@qq.com>
2019-07-27 15:05:43 +0800

    Add files via upload

--- tk/tk_semicircle.py
+++ tk/tk_semicircle.py
@@ -1,15 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jul  6 16:21:22 2019
-
-@author: Robin
-"""
-
 from tkinter import *
 tk = Tk()
-def semicircle():
-    cav.create_arc(250, 10, 250, 260, extent = 180, style = ARC)
+tk.title('create a semicircle')
+def up():
+    cav.create_arc(10, 100, 490, 490, extent = 180, style = ARC)
+
+def down():
+    cav.create_arc(10, 100, 490, 490, extent = -180, style = ARC)
+
+def egg():
+    up()
+    down()
+
+def clear():
+    cav.create_rectangle(0, 0, 500, 500, fill = 'white', outline = 'white')
+
 cav = Canvas(tk, width = 500, height = 500)
-btn = Button(tk, text = 'create cemicircle', command = semicircle)
+btn1 = Button(tk, text = 'create cemicircle(up)', command = up).pack()
+btn2 = Button(tk, text = 'create cemicircle(down)', command = down).pack()
+btn3 = Button(tk, text = 'create an big egg', command = egg).pack()
+btn4 = Button(tk, text = 'clear', command = clear).pack()
 cav.pack()
-btn.pack()
+tk.mainloop()
+
+
