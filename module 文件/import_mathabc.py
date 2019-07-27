955ede1fd862143ca95e266476b9ea2fdce72994
git4robot <49167781@qq.com>
2019-07-27 14:23:26 +0800

    Add files via upload

--- module 文件/import_mathabc.py
+++ module 文件/import_mathabc.py
@@ -1,5 +1,9 @@
-import modules.mathabc as m
+import mathabc as m
 m.add(245879,259364)
 m.minus(92876,54444)
-m.addadd(254,23)
-m.divide(523956327,125)
+m.multiplication(254,23)
+m.divide(527,125)
+m.pow(25,25)
+m.floordiv(749352,578)
+m.delivery(56,6)
+m.divide(5,0)
