dc0e98f108f4656ddec5cbd89b2825c35cd77d17
git4robot <49167781@qq.com>
2019-07-27 14:10:48 +0800

    Add files via upload

--- module 文件/mathabc.py
+++ module 文件/mathabc.py
@@ -1,79 +1,84 @@
 '''A module to calculate numbers.
-Version -- 2.5.5(at python 3+)'''
-
-__all__ = ['InputError', 'add', 'minus', 'addadd', 'divide']
+It takes function add、function minus、function multiplication、function divide、function pow、function floordiv and function delivery.'''
 
 class InputError(BaseException):
+    '''Invalid input.''' 
     pass
 
 def add(n1, n2):
-    '''print n1 + n2'''
+    '''print n1 + n2.'''
     try:
         n1 + n2
-    except Exception as e:
-        print('Invalid input: %s' % e)
+    except Exception:
+        raise InputError from None
     else:
         print('%s + %s = %s' % (n1, n2, n1 + n2))
 
 def minus(n1, n2):
-    '''print n1 - n2'''
+    '''print n1 - n2.'''
     try:
         n1 - n2
-    except Exception as e:
-        print('Invalid input: %s' % e)
+    except Exception:
+        raise InputError from None
     else:
         print('%s - %s = %s' % (n1, n2, n1 - n2))
 
-def addadd(n1, n2):
-    '''print n1 * n2'''
+def multiplication(n1, n2):
+    '''print n1 * n2.'''
     try:
         n1 * n2
-    except Exception as e:
-        print('Invalid input: %s' % e)
+    except Exception:
+        raise InputError from None
     else:
         print('%s * %s = %s' % (n1, n2, n1 * n2))
 
 def divide(n1, n2):
-    '''print n1 / n2'''
+    '''print n1 / n2.'''
     try:
         n1 / n2
-    except Exception as e:
-        print('Invalid input: %s' % e)
+    except Exception:
+        raise InputError from None
     else:
         if n1 / n2 == round(n1 / n2):
-            print('%s / %s = %s' % (n1, n2, n1 // n2))
+            print('%s / %s = %s' % (n1, n2, round(n1 / n2)))
         else:
-            print('%s / %s = %s ...... %s' % (n1, n2, n1 // n2, n1 - (n1 // n2) * n2))
-            assert n1 - (n1 // n2) * n2 < n2
+            print('%s / %s = %s ...... %s' % (n1, n2, round(n1 / n2), n1 - (round(n1 / n2) * n2)))
 
-def addaddadd(n1, b):
-    '''print n1 ** n2'''
+def pow(n1, b):
+    '''print n1 ** n2.'''
     try:
         n1 ** b
-    except Exception as e:
-        print('Invalid input: %s' % e)
+    except Exception:
+        raise InputError from None
     else:
         print('%s ** %s = %s' % (n1, b, n1 ** b))
 
-def divide_no_last(n1, n2):
-    '''print n1 // n2'''
+def floordiv(n1, n2):
+    '''print n1 // n2.'''
     try:
         n1 // n2
-    except Exception as e:
-        print('Invalid input: %s' % e)
+    except Exception:
+        raise InputError from None
     else:
         print('%s // %s = %s' % (n1, n2, n1 // n2))
 
-def divide_last(n1, n2):
-    '''print n1 % n2'''
+def delivery(n1, n2):
+    '''print n1 % n2.'''
     try:
         n1 % n2
-    except Exception as e:
-        print('Invalid input: %s' % e)
+    except Exception:
+        raise InputError from None
     else:
         print('%s % %s = %s' % (n1, n2, n1 % n2))
 
+
+'''Test for mathabc.'''
+def _test():
+    divide(15, 6)
+    divide(15, 'h')
+
 if __name__ == "__main__":
-    divide(15, 'g')
+    _test()
+
 
 
